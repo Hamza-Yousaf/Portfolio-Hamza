d40a03b9b0a5b260588527cc7c55424d73faa372
Hamza Yousaf <Hamzayousaf933@gmail.com>
2019-12-10 00:25:05 +0500

    Initial commit

--- pages/index.js
+++ pages/index.js
@@ -1,88 +1,68 @@
-import React from 'react'
-import Head from 'next/head'
-import Nav from '../components/nav'
+import React from 'react';
+import BaseLayout from '../components/layouts/BaseLayout';
+import axios from 'axios';
 
-const Home = () => (
-  <div>
-    <Head>
-      <title>Home</title>
-      <link rel="icon" href="/favicon.ico" />
-    </Head>
+import SuperComponent from '../components/SuperComponent';
 
-    <Nav />
+class Index extends SuperComponent {
 
-    <div className="hero">
-      <h1 className="title">Welcome to Next.js!</h1>
-      <p className="description">
-        To get started, edit <code>pages/index.js</code> and save to reload.
-      </p>
+  static async getInitialProps() {
+    let userData = {};
 
-      <div className="row">
-        <a href="https://nextjs.org/docs" className="card">
-          <h3>Documentation &rarr;</h3>
-          <p>Learn more about Next.js in the documentation.</p>
-        </a>
-        <a href="https://nextjs.org/learn" className="card">
-          <h3>Next.js Learn &rarr;</h3>
-          <p>Learn about Next.js by following an interactive tutorial!</p>
-        </a>
-        <a
-          href="https://github.com/zeit/next.js/tree/master/examples"
-          className="card"
-        >
-          <h3>Examples &rarr;</h3>
-          <p>Find other example boilerplates on the Next.js GitHub.</p>
-        </a>
-      </div>
-    </div>
+    try {
+      const response = await axios.get('https://jsonplaceholder.typicode.com/todos/1');
+      userData = response.data;
+    } catch(err) {
+      console.error(err);
+    }
 
-    <style jsx>{`
-      .hero {
-        width: 100%;
-        color: #333;
-      }
-      .title {
-        margin: 0;
-        width: 100%;
-        padding-top: 80px;
-        line-height: 1.15;
-        font-size: 48px;
-      }
-      .title,
-      .description {
-        text-align: center;
-      }
-      .row {
-        max-width: 880px;
-        margin: 80px auto 40px;
-        display: flex;
-        flex-direction: row;
-        justify-content: space-around;
-      }
-      .card {
-        padding: 18px 18px 24px;
-        width: 220px;
-        text-align: left;
-        text-decoration: none;
-        color: #434343;
-        border: 1px solid #9b9b9b;
-      }
-      .card:hover {
-        border-color: #067df7;
-      }
-      .card h3 {
-        margin: 0;
-        color: #067df7;
-        font-size: 18px;
-      }
-      .card p {
-        margin: 0;
-        padding: 12px 0 0;
-        font-size: 13px;
-        color: #333;
-      }
-    `}</style>
-  </div>
-)
+    return {initialData: [1,2,3,4], userData};
+  }
 
-export default Home
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      title: 'I am Index Page'
+    }
+  }
+
+  componentDidMount() {
+    console.log('componentDidMount');
+  }
+
+  componentDidUpdate() {
+    console.log();
+    console.log('componentDidUpdate');
+  }
+
+  componentWillUnmount() {
+    console.log('componentWillUnmount');
+  }
+
+  updateTitle = () => {
+    this.setState({title: 'I am Updated Index Page'});
+  }
+
+  render() {
+    const { title } = this.state;
+    const { userData, initialData } = this.props;
+
+    // const title = this.state.title;
+
+    return (
+      <BaseLayout>
+        <h1 className='fromPage'> I am Index Page from Class Component</h1>
+        <h2> { title } </h2>
+        <h2> { userData.title } </h2>
+        <button onClick={this.updateTitle}> Change Title </button>
+      </BaseLayout>
+    )
+  }
+}
+
+
+
+export default Index;
+
